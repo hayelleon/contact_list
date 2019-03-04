c001160593cacf999f79935097b0465d6ddfa0c3
hayelleon <hayelleon@gmail.com>
2019-03-03 22:36:59 -0300

    User Authentication implemented and working! User page and app locker to signed_in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root            to: 'static_pages#index'
   get 'sobre',    to: 'static_pages#sobre'
   get 'contato',  to: 'static_pages#contato'
